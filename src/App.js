f350a67ba9ad058df30749a0f5382ab755a211bc
Huc <phuc92147@gmail.com>
2024-10-09 00:00:29 +0700

    updateComponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 // src/App.tsx
 import React, { useState } from 'react';
 import './App.css'; 
-import TicketBooking from './components/TicketBooking'; // Nhập component TicketBooking
-
+import TicketBooking from './components/TicketBooking'; 
+import ManageBooking from './components/BookingManagement';
+import Checkin from './components/Checkin'; 
 const App: React.FC = () => {
     const [showBooking, setShowBooking] = useState(false); // State để theo dõi việc hiển thị giao diện mua vé
 
     const handleBookingClick = () => {
-        setShowBooking(true); // Hiển thị giao diện mua vé khi nhấp vào
+        setShowBooking(true); 
+    };
+
+    const handleBookingManagement = () => {
+        setShowBooking(true); 
+    };
+
+    const handleCheckin = () => {
+        setShowBooking(true); 
     };
 
     return (
@@ -16,8 +25,8 @@ const App: React.FC = () => {
                 <h1>PHUC's plane</h1>
                 <nav>
                     <a href="#" onClick={handleBookingClick}>Mua vé</a>
-                    <a href="#">Quản lý đặt chỗ</a>
-                    <a href="#">Làm thủ tục</a>
+                    <a href="#" onClick={handleBookingManagement}>Quản lý đặt chỗ</a>
+                    <a href="#" onClick={handleCheckin}>Làm thủ tục</a>
                 </nav>
             </header>
 
@@ -29,8 +38,10 @@ const App: React.FC = () => {
                 </div>
             </div>
 
-            {showBooking && <TicketBooking />} {/* Hiển thị giao diện mua vé nếu showBooking là true */}
-
+            {showBooking && <TicketBooking />} 
+            {showBooking && <ManageBooking />}
+            {showBooking && <Checkin />}
+             
             <footer className="footer">
                 <p>&copy; 2023 PHUC's plane. Tất cả quyền được bảo lưu.</p>
             </footer>
